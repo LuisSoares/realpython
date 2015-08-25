06a263de1e62fac100fcfaedfab5407217d9c82c
LuisSoares <luis.miguel.mendes.soares@gmail.com>
2015-08-24 20:57:31 -0400

    dynamic

--- app.py
+++ app.py
@@ -2,17 +2,22 @@
 #---- Flask Hello World-----#
 from flask import Flask
 #create the application object
-application=Flask(__name__)
+app=Flask(__name__)
 
 #use decorators to link the function to a url
-@application.route("/")
-@application.route("/hello")
+@app.route("/")
+@app.route("/hello")
 
 #define the view using a function which returns a string
 
 def hello_world():
     return "Hello,World!"
+
+#dynamic route
+@app.route("/test")
+def search():
+    return "Hello"
     
 if __name__=="__main__":
-    application.run()
+    app.run()
     
